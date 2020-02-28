dd6b147faab0c798aa922211071e4aae83ae15ee
Renato Babata <37925829+renatobabata@users.noreply.github.com>
2020-02-28 12:30:47 -0300

    Desarrollo Web con Spring 5 - 58
    Desarrollo Web con Spring 5 - 58

---
+++ cineapp/src/main/java/net/itinajero/app/controller/FilmesController.java
@@ -0,0 +1,16 @@
+package net.itinajero.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filmes")
+public class FilmesController {
+
+	@GetMapping("/create")
+	public String criar() {
+		return "filmes/formFilme";
+		
+	}
+}
