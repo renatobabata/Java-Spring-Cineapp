292154606b965152ce5aaeddaca7391c6658bca9
Renato Babata <37925829+renatobabata@users.noreply.github.com>
2020-03-02 12:26:51 -0300

    Desarrollo Web con Spring 5 - 62
    Desarrollo Web con Spring 5 - 62

--- cineapp/src/main/java/net/itinajero/app/controller/FilmesController.java
+++ cineapp/src/main/java/net/itinajero/app/controller/FilmesController.java
@@ -1,8 +1,13 @@
 package net.itinajero.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.itinajero.app.model.Filme;
 
 @Controller
 @RequestMapping("/filmes")
@@ -12,5 +17,22 @@ public class FilmesController {
 	public String criar() {
 		return "filmes/formFilme";
 		
+	}
+	
+	@PostMapping("/save")
+	public String guardar(Filme filme, BindingResult result) {
+		
+		/*
+		if(result.hasErrors()) {
+			System.out.println("Existiram erros");
+			return"filmes/formFilme";
+		}
+		*/
+		for(ObjectError error: result.getAllErrors()) {
+			System.out.println("Mensagem de erro: "+error.getDefaultMessage());
+		}
+		
+		System.out.println("Guardar filmes "+filme);
+		return "filmes/formFilme";
 	}
 }
